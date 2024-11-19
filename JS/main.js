559e729d0f29b942056c97d4ac086e61b43b88d0
leoFAP <leonardoalegre2008@cesanrafael.org>
2024-11-19 14:24:25 -0400

    javasripkrrge

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+
+function sumar(){
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado=numerouno+numerodos+numerotres;
+alert("la suma es "+ resultado);
+}
+function restar(){
+    let numerouno=2;
+    let numerodos=4;
+    let numerotres=10;
+    let resultado=numerouno-numerodos-numerotres;
+    alert("la resta es "+ resultado);
+    }
+    function multiplicar(){
+        let numerouno=2;
+        let numerodos=4;
+        let numerotres=10;
+        let resultado=numerouno*numerodos*numerotres;
+        alert("la multiplicacion es "+ resultado);
+        }
+        function dividir(){
+            let numerouno=2;
+            let numerodos=4;
+            let numerotres=10;
+            let resultado=numerouno/numerodos/numerotres;
+            alert("la division es "+ resultado);
+            }
+
+            
+        
+    
+
